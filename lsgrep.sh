47dfb65fc6b9211d3bd65ac9bc1c6639a62c8476
RENoMafex <Mafexxx@aol.com>
2024-08-29 20:49:47 +0200

    Fixed color support of ls in lsgrep.sh

--- lsgrep.sh
+++ lsgrep.sh
@@ -9,13 +9,13 @@ else
 fi
 
 if [[ -z "$input" && "$grc" == "true" ]]; then
-	grc ls -lAh --color=auto
+	grc ls -lAh --color=always
 elif [[ -z "$input" && "$grc" == "false" ]]; then
-	ls -lAh --color=auto
+	ls -lAh --color=always
 elif [[ -n "$input" && "$grc" == "true" ]]; then
-	ls -lAh --color=auto | grc -c conf.ls grep $input
+	ls -lAh --color=always | grc -c conf.ls grep $input
 elif [[ -n "$input" && "$grc" == "false" ]]; then
-	ls -lAh --color=auto | grep $input
+	ls -lAh --color=always | grep $input
 else
 	echo "Something went wrong!"
 	exit 1
