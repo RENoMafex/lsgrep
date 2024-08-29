2578958ad6d6e6b1392cd36417268364eca8e7e6
RENoMafex <Mafexxx@aol.com>
2024-08-29 20:44:13 +0200

    changed lsgrep.sh code to work different ways if grc is installed and if an argument was given

--- lsgrep.sh
+++ lsgrep.sh
@@ -3,7 +3,20 @@
 input="$1"
 
 if command -v grc >/dev/null; then
-	ls -lAh --color=always | grc -c conf.ls grep .$input
+	grc="true"
 else
-	ls -lAh --color=always | grep .$input
+	grc="false"
+fi
+
+if [ -z "$input" && "$grc" == "true" ]; then
+	grc ls -lAh --color=auto
+elif [ -z "$input" && "$grc" == "false" ]; then
+	ls -lAh --color=auto
+elif [ -n "$input" && "$grc" == "true" ]; then
+	ls -lAh --color=auto | grc -c conf.ls grep $input
+elif [ -n "$input" && "$grc" == "false" ]; then
+	ls -lAh --color=auto | grep $input
+else
+	echo "Something went wrong!"
+	exit 1
 fi
