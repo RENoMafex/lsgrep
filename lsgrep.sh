171c313f2454384cfc5cf8adbebc41014afb8a79
RENoMafex <Mafexxx@aol.com>
2024-08-29 20:47:10 +0200

    fixed lsgrep.sh

--- lsgrep.sh
+++ lsgrep.sh
@@ -8,13 +8,13 @@ else
 	grc="false"
 fi
 
-if [ -z "$input" && "$grc" == "true" ]; then
+if [[ -z "$input" && "$grc" == "true" ]]; then
 	grc ls -lAh --color=auto
-elif [ -z "$input" && "$grc" == "false" ]; then
+elif [[ -z "$input" && "$grc" == "false" ]]; then
 	ls -lAh --color=auto
-elif [ -n "$input" && "$grc" == "true" ]; then
+elif [[ -n "$input" && "$grc" == "true" ]]; then
 	ls -lAh --color=auto | grc -c conf.ls grep $input
-elif [ -n "$input" && "$grc" == "false" ]; then
+elif [[ -n "$input" && "$grc" == "false" ]]; then
 	ls -lAh --color=auto | grep $input
 else
 	echo "Something went wrong!"
