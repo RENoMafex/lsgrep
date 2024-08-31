3a21e54c229a62b8c19e9f0d7c464e5041be26a3
RENoMafex <Mafexxx@aol.com>
2024-08-31 14:01:57 +0200

    Made install.sh work properly

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 echo "Welcome to the Installation of lsgrep!"
 echo ""
-read -r -p "Do you want to install Systemwide (could need sudo) or only for user \"$USER\" [s/U] " response
+read -r -p "Do you want to install Systemwide (might need sudo) or only for user \"$USER\" [s/U] " response
 if [[ "$response" =~ ^([sS])$ ]]; then
 	INSTALLDIR="/usr/local/bin"
 else
@@ -17,8 +17,19 @@ cp -fv lsgrep.sh $INSTALLDIR/lsgrep
 echo "done!"
 echo ""
 
-read -r -p "Do you want to alias \"lg\" to \"lsgrep\" [y/N] " response
+read -r -p "Do you want to alias \"lg\" to \"lsgrep\" (only for user $USER) [y/N] " response
 if [[ "$response" =~ ^([yY])$ ]]; then
 	echo 'alias lg="lsgrep"' >>$HOME/.profile
+	echo "done!"
 fi
-echo "done!"
+echo ""
+
+read -r -p "Do you want to Remove the installation files? [y/N] " response
+if [[ "$response" =~ ^([yY])$ ]]; then
+	selfdir=$(dirname "$(realpath "$0")")
+	rm -rfv $selfdir
+	echo "done!"
+	echo ""
+fi
+
+echo "Thank you for installing lsgrep"
